1dfd6ed149fd8e8780c5c2bbae549ff6d03c8154
s40523219 <40523219@gm.nfu.edu.tw>
2018-12-04 22:38:24 +0800

    add start.py (v1.0.0)

--- vrep_test/vrep_setting.py
+++ vrep_test/vrep_setting.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 import sys
-import math
-import time
+from math import sqrt, pi
+from time import sleep
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from .Ui_vrep_setting import Ui_Dialog
 from ..vrep_remoAPI import vrep
 from ..gcode.gcodeParser import *
+import vrep
 
-la = 347
-to = 30
-pa = 40
-sp = 235
 
 class readingGcode(QThread):
     def __init__(self,getProgress, layers,  parent=None):
@@ -47,12 +44,12 @@ class vrepsetting(QDialog, Ui_Dialog):
     def __init__(self, parent=None):
         super(vrepsetting, self).__init__(parent)
         self.setupUi(self)
-        self.initForms()
+        self.init_form()
         # child threaded script: 
         # 內建使用 port 19997 若要加入其他 port, 在  serve 端程式納入
         #simExtRemoteApiStart(19999)
     
-    def initForms(self): 
+    def init_form(self): 
         self.openGcodeFile.clicked.connect(self.__open_file__)
         self.translate.clicked.connect(self.ik_transform)
     
@@ -93,9 +90,9 @@ class vrepsetting(QDialog, Ui_Dialog):
         ab = math.sqrt((b2x-b1x)*(b2x-b1x)+(b2y-b1y)*(b2y-b1y))
         hb = math.sqrt(la*la-ab*ab)
 
-        ac = math.sqrt((c2x-c1x)*(c2x-c1x)+(c2y-c1y)*(c2y-c1y))
+        ac = sqrt((c2x-c1x)*(c2x-c1x)+(c2y-c1y)*(c2y-c1y))
         #print(ac)
-        hc = math.sqrt(la*la-ac*ac)
+        hc = sqrt(la*la-ac*ac)
 
         a2z = tz+to+ha
         b2z = tz+to+hb
@@ -103,7 +100,7 @@ class vrepsetting(QDialog, Ui_Dialog):
         
         print(a2z,"/n",b2z,"/n",c2z)
         self.translateShow.addItem("(X: {:.04f},Y: {:.04f},Z: {:.04f})".format(a2z, b2z, c2z))
-    
+   
     
     def __open_file__(self):
         filename, _ = QFileDialog.getOpenFileName(self, "Open Files", '..', 'GCode(*.gcode)')
@@ -131,7 +128,7 @@ class vrepsetting(QDialog, Ui_Dialog):
         clientID = vrep.simxStart('127.0.0.1', 19998, True, True, 5000, 5)
         if clientID!= -1:
             #print("Connected to remote server")
-            time.sleep(0.5)
+            sleep(0.5)
             errorCode,plate=vrep.simxGetObjectHandle(clientID,'plate',vrep.simx_opmode_oneshot_wait)
             
             if errorCode == -1:
@@ -142,18 +139,7 @@ class vrepsetting(QDialog, Ui_Dialog):
         else:
             print('Connection not successful')
             sys.exit('Could not connect')
-        '''
-        time.sleep(0.5)
-        errorCode,plate=vrep.simxGetObjectHandle(clientID,'plate',vrep.simx_opmode_streaming)
-        
-        
-        if errorCode == -1:
-            #print('Can not find plate')
-            sys.exit()                
-        errorCode=vrep.simxSetObjectPosition(clientID,plate,-1,[e/1000,r/1000,t/1000+0.1165], vrep.simx_opmode_streaming)
-        #print(e/1000,r/1000,t/1000)
-        '''
-        
+
     def renderVertices(self):
         work = readingGcode(self.progressBar, self.model.layers)
         work.run()
@@ -168,7 +154,7 @@ class vrepsetting(QDialog, Ui_Dialog):
         test = []
         vrep.simxFinish(-1)         
         clientID = vrep.simxStart('127.0.0.1', 19999, True, True, 5000, 5)
-        deg = math.pi/180
+        deg = pi/180
         if clientID!=-1: 
             print("Connected to remote server")
         else:
@@ -185,27 +171,3 @@ class vrepsetting(QDialog, Ui_Dialog):
         
         print(test)
         print("test")
-        #i =360
-        #for i in range(360):
-        #    errorCode = vrep.simxSetJointPosition(clientID, Xaxis_joint,i*deg, vrep.simx_opmode_oneshot_wait)
-        #def setJointPosition(incAngle, steps):
-        #    for i  in range(steps): errorCode = vrep.simxSetJointPosition(clientID, jointx, i*incAngle*deg, vrep.simx_opmode_oneshot_wait)
-        
-        #setJointPosition(10, 720)
-        
-
-'''        
--- Put some initialization code here:
-simSetThreadSwitchTiming(2) -- Default timing for automatic thread switching
-simExtRemoteApiStart(19999)
-
-
--- Here we execute the regular thread code:
-res,err=xpcall(threadFunction,function(err) return debug.traceback(err) end)
-if not res then
-    simAddStatusbarMessage('Lua runtime error: '..err)
-end
-
--- Put some clean-up code here:
-'''
- 
